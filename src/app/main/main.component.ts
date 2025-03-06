3ff2e64e4e871f8b93bfea38177cad1fce0a5d89
Daniel <danieldiazmc@gmail.com>
2025-03-06 08:18:57 -0400

    update dist

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,8 +1,9 @@
-import { Component, HostBinding} from '@angular/core';
+import { Component} from '@angular/core';
 import { THEME_UI } from '../shared/enums/theme-ui.enum';
 import { LOCAL_STORAGE_KEY } from '../shared/enums/local-storage-key.enum';
 import { TokenSpotify } from '../shared/interface/spotify.interface';
 import { SpotifyService } from '../core/spotify.service';
+import { ThemeService } from '../core/theme.service';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,24 +11,18 @@ import { SpotifyService } from '../core/spotify.service';
 })
 export class MainComponent {
 
-  @HostBinding('class') className = ''; 
+  public isDarkTheme:boolean;
 
-  public constructor(private spotifyService:SpotifyService){
-    let theme = window.localStorage.getItem(LOCAL_STORAGE_KEY.THEME_UI);
-    if(theme){      
-      this.className = theme;
-    }else{
-      this.onToggleControlSwitch(false);
-    }
-  }
+  public constructor(public themeService: ThemeService, private spotifyService:SpotifyService){}
 
   ngOnInit() {
+    const currentTheme = window.localStorage.getItem(LOCAL_STORAGE_KEY.THEME_UI);
+    this.isDarkTheme = (currentTheme === THEME_UI.DARK)
     this.intervalGetTokenSpotify();    
   }
 
-  public onToggleControlSwitch(darkMode:boolean){
-    this.className = darkMode ? THEME_UI.DARK : THEME_UI.LIGHT;
-    window.localStorage.setItem(LOCAL_STORAGE_KEY.THEME_UI,this.className);
+  public onChangeIsDarkTheme(isDarkTheme:boolean){
+    this.themeService.toggleTheme(isDarkTheme ? THEME_UI.DARK : THEME_UI.LIGHT);
   }
 
   public intervalGetTokenSpotify(){    
